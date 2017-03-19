d0b5f1fcb5f964dedbd31801b6892a8ba74609e0
Alexey Vasilyev <bestspajic@gmail.com>
2017-03-19 21:51:22 +0300

    Make links to blog work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope controller: :home do
-    get '/', action: 'index'
+    get '/', action: 'index', as: :root
   end
 
   scope controller: :blog_posts do
